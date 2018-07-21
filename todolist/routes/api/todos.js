81322bd5d7bf0b5d547f6c7057016fb5a01c4424
Roger Castro <omarcastro18@gmail.com>
2018-07-21 06:05:01 -0300

    todos routes for CRUD

--- todolist/routes/api/todos.js
+++ todolist/routes/api/todos.js
@@ -1,6 +1,44 @@
-const express = require("express");
-const router = express.Router();
-const mongoose = require("mongoose");
+router.get("/todo", function(req, res, nextMiddleWare) {
+  res.send({ type: "GET" });
+});
 
-// Todo model
-const Todo = require("../../models/Todo");
+// Add new todo
+router.post("/todo", function(req, res, nextMiddleWare) {
+  console.log(req.body);
+
+  // var todo = new Todo(req.body);
+  // todo.save();
+  // Mongoose method for this is:
+  Todo.create(req.body)
+    .then(function(todo) {
+      res.send(todo);
+    })
+    .catch(nextMiddleWare);
+
+  // res.send({
+  //   type:'POST',
+  //   todo: req.body.todo
+  // });
+});
+
+// Update todo
+router.put("/todo/:id", function(req, res, nextMiddleWare) {
+  Todo.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function() {
+    Todo.findOne({ _id: req.params.id }).then(function(todo) {
+      res.send(todo);
+    });
+  });
+  // res.send({type:'PUT'});
+});
+
+router.delete("/todo/:id", function(req, res, nextMiddleWare) {
+  // console.log(req.params.id);
+
+  // Another mongoose method
+  Todo.findByIdAndRemove({ _id: req.params.id }).then(function(todo) {
+    res.send(todo);
+  });
+  // res.send({type:'DELETE'});
+});
+
+module.exports = router;
