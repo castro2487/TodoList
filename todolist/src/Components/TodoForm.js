ddd8b5f40f52e22f6795102f3ce4239914052e33
Roger Castro <omarcastro18@gmail.com>
2018-07-21 04:53:29 -0300

    adding a form to receive all the input

---
+++ todolist/src/Components/TodoForm.js
@@ -0,0 +1,24 @@
+const TodoForm = ({ addTodo }) => {
+  // Input tracker
+  let input;
+
+  return (
+    <div>
+      <input
+        ref={node => {
+          input = node;
+        }}
+      />
+      <button
+        onClick={() => {
+          addTodo(input.value);
+          input.value = "";
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default TodoForm;
