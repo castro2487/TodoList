c2e4f9ddba7aec706f46a64cfb397840fb13656e
Roger Castro <omarcastro18@gmail.com>
2018-07-21 05:05:56 -0300

    addind react dependency for each component

--- todolist/src/Components/TodoForm.js
+++ todolist/src/Components/TodoForm.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const TodoForm = ({ addTodo }) => {
   // Input tracker
   let input;
--- todolist/src/Components/TodoList.js
+++ todolist/src/Components/TodoList.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Todo from "./TodoItem";
 
 const TodoList = ({ todos, remove }) => {
