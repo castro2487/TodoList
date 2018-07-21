dfaeafe986278dd047032e41c7f66c3c69540f47
Roger Castro <omarcastro18@gmail.com>
2018-07-21 06:00:59 -0300

    starting to model server

---
+++ todolist/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Use Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
