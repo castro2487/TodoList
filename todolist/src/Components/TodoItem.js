6bcc1a77f7f15ed841ed0625be23943a0d61fc10
Roger Castro <omarcastro18@gmail.com>
2018-07-21 05:01:48 -0300

    add item element of the list of todo

---
+++ todolist/src/Components/TodoItem.js
@@ -0,0 +1,13 @@
+const Todo = ({ todo, remove }) => {
+  // Each Todo
+  return (
+    <li
+      onClick={() => {
+        remove(todo.id);
+      }}
+    >
+      {todo.text}
+    </li>
+  );
+};
+export default Todo;
