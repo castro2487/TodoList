e76b04eaf68ea87bb9cda0692e9e40bda429af5f
Roger Castro <omarcastro18@gmail.com>
2018-07-21 06:05:08 -0300

    required data model

--- todolist/models/todos.js
+++ todolist/models/todos.js
@@ -1,11 +1,22 @@
+// Adds a layer of methods to easily save, edit retreive and delete data from the MongoDB
+// Allows us to create our Models and Schemas easily
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// create todo Schema & model
 const TodoSchema = new Schema({
-  text: {
+  task: {
     type: String,
-    required: true
+    required: [true, "Todo field is required"]
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false
   }
 });
 
-module.exports = Todo = mongoose.model("todo", TodoSchema);
+// Todo model represents the todo collection in the database
+// MongDB will pluralize 'todo' to 'todo's' collection
+const Todo = mongoose.model("todo", TodoSchema);
+
+module.exports = Todo;
