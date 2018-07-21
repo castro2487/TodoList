957d4bee8bd89253664437de0dad2bff76f8e1d5
Roger Castro <omarcastro18@gmail.com>
2018-07-21 05:02:05 -0300

    list of all the todo that are going to be added

---
+++ todolist/src/Components/TodoList.js
@@ -0,0 +1,11 @@
+import Todo from "./TodoItem";
+
+const TodoList = ({ todos, remove }) => {
+  // Map through the todos
+  const todoNode = todos.map(todo => {
+    return <Todo todo={todo} key={todo.id} remove={remove} />;
+  });
+  return <ul>{todoNode}</ul>;
+};
+
+export default TodoList;
